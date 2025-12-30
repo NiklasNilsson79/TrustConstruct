507919c68330f77c369e79627323691571432bd3
NiklasNilsson79 <nelzonproduction@gmail.com>
2025-12-30 23:05:25 +0100

    Fix forge-std import path in deploy script

--- scripts/DeployReportRegistry.s.sol
+++ scripts/DeployReportRegistry.s.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "forge-std/Script.sol";
+import { Script } from "../lib/forge-std/src/Script.sol";
+
 import "contracts/ReportRegistry.sol";
 
 
